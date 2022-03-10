cf943fdb0dd973159486cbf813930b1c0916eb87
Hushino <abde95.inf3@gmail.com>
2022-03-10 16:00:27 +0100

    second_commit

--- js/index.js
+++ js/index.js
@@ -13,17 +13,26 @@ fetchMoviesJSON().then(movies => {
     for (let index = 0; index < movies.peliculas.length; index++) {
        
         let title = movies.peliculas[index].nombre;
-        let director = movies.peliculas[index].director;
+        let original_name = movies.peliculas[index].original_name;
+        let año = movies.peliculas[index].año
+        let clasificacion = movies.peliculas[index].clasificacion
         let img = movies.peliculas[index].img;
             moviesSection.innerHTML += `
-                <div class="card" style="width: 18rem;">
+            
+            
+            
+            <div class="card border-dark mb-3 container rounded" style="max-width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">${title}</h5>
-                        <img class="card-img-top" src="${img}" alt="Card image cap">
-                        <h5 class="card-title">${director}</h5>
-                        
+                        <h5 class="card-header o-name">${original_name}</h5>
+                        <img class="card-img-top img-fluid" src="${img}" alt="Card image cap">
+                        <h6 class="card-title">${title}</h6>
+                        <h8 class="card-title">${clasificacion}</h8>
+                        <h7 class="card-title">${año}</h7>                
+                    </div>
                 </div>
-            </div>
+            
+            
+           
             `
         
         
